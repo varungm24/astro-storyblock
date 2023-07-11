5ada54d040c7a7be46679f53ab65cf52aa065b2b
GM Varun <gmvarun@GMs-MacBook-Air.local>
2023-07-11 16:08:59 +0530

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from "astro/config";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
+
+const env = loadEnv("", process.cwd(), "STORYBLOK");
+
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        blogPost: "storyblok/BlogPost",
+        blogPostList: "storyblok/BlogPostList",
+        page: "storyblok/Page",
+      },
+      apiOptions: {
+        // Choose your Storyblok space region
+        region: "eu", // optional,  or 'eu' (default)
+      },
+    }),
+  ],
+});
